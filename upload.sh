84257a59bf7bdfb3b36f23f76b65d6e282095956
Sushrut1101 <guptasushrut@gmail.com>
2021-12-15 02:45:00 +0530

    Only Print the Download Link
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- upload.sh
+++ upload.sh
@@ -13,7 +13,9 @@ SERVER=$(curl -s https://apiv2.gofile.io/getServer | jq  -r '.data|.server')
 
 UPLOAD=$(curl -F file=${FILE} https://${SERVER}.gofile.io/uploadFile)
 
+LINK=$(echo $UPLOAD | jq -r '.data|.downloadPage')
+
 # Print the link!
-echo $UPLOAD
+echo $LINK
 
 echo " "
